a6b4954f9f2613a21653ce8daf1b0f1f135e1cda
ErolBa <balkovic.erol@gmail.com>
2023-05-03 10:07:09 +0200

    3d plotting

--- hmhd_scripts/plot_hmhd_3d_scalar.py
+++ hmhd_scripts/plot_hmhd_3d_scalar.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-import numpy as np
-from py_spec import HMHDslab
 import matplotlib
 matplotlib.use("Qt5Agg")
-import matplotlib.pyplot as plt
+from py_spec import HMHDslab
 
-plt.rcParams['figure.figsize'] = [7., 7]
-
-HMHDslab.plot_scalar_3d(sys.argv[1], sys.argv[2], ylims=[-np.pi, np.pi])
-
-plt.tight_layout()
-plt.show(block=True)
+HMHDslab.plot_scalar_3d(sys.argv)
