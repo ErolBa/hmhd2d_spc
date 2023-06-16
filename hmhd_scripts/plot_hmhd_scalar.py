46072dc53a8a166b6be22c073eacf24f4f1fb009
ErolBa <balkovic.erol@gmail.com>
2023-06-16 14:41:30 +0200

    scripts 2

--- hmhd_scripts/plot_hmhd_scalar.py
+++ hmhd_scripts/plot_hmhd_scalar.py
@@ -1,21 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-import numpy as np
-from py_spec import HMHDslab
 import matplotlib
 matplotlib.use("Qt5Agg")
-import matplotlib.pyplot as plt
+from py_spec import HMHDslab
 
-plt.rcParams['figure.figsize'] = [7., 7]
-
-num_args = len(sys.argv)
-
-if(num_args<3):
-    raise ValueError("Not enough input arguments!")
-else:
-    for n in range(2, num_args):
-        HMHDslab.plot_scalar(sys.argv[n], sys.argv[1], ylims=[-np.pi, np.pi])
-        plt.tight_layout()
-
-plt.show(block=True)
+HMHDslab.plot_scalar_2d(sys.argv)
